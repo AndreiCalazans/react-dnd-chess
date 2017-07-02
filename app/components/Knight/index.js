f19c0b8989ad3d4f1f1694af2d5085290d87389a
AndreiCalazans <andreixoc@hotmail.com>
2017-07-02 11:36:55 -0300

    finished

--- app/components/Knight/index.js
+++ app/components/Knight/index.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ItemTypes } from '../../Constant';
+import { DragSource } from 'react-dnd';
 
-export default class Knight extends React.Component {
+const knightSource = {
+  beginDrag(props) {
+    return {};
+  }
+};
+
+
+function collect(connect, monitor) {
+  return {
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging(),
+  };
+}
+
+
+@DragSource(ItemTypes.KNIGHT, knightSource, collect)
+export default class Knight extends Component {
+  static propTypes = {
+    connectDragSource: PropTypes.func.isRequired,
+    isDragging: PropTypes.bool.isRequired,
+  };
+
   render() {
-    return <span style={{ fontSize: '24px' }}>♘</span>;
+    const { connectDragSource, isDragging } = this.props;
+    return connectDragSource(
+      <div style={{
+        opacity: isDragging ? 0.5 : 1,
+        fontSize: 25,
+        fontWeight: 'bold',
+        cursor: 'move',
+      }}
+      >
+        ♘
+      </div>,
+    );
   }
 }
--- app/containers/Board.js
+++ app/containers/Board.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import Square from '../components/Square';
 import Knight from '../components/Knight';
 import { moveKnight, canMoveKnight } from '../Game';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import BoardSquare from '../components/BoardSquare';
 
+@DragDropContext(HTML5Backend)
 export default class Board extends React.Component {
   static propTypes = {
     knightPosition: PropTypes.arrayOf(
@@ -18,29 +22,25 @@ export default class Board extends React.Component {
   }
 
   renderSquare(i) {
-    // interlaying black and white boxes
     const x = i % 8;
     const y = Math.floor(i / 8);
-    const black = (x + y) % 2 === 1;
-
-    const [knightX, knightY] = this.props.knightPosition;
-    const piece = (x === knightX && y === knightY) ?
-      <Knight /> :
-      null;
-
     return (
-      <div
-        key={i}
-        style={{ width: '12.5%', height: '12.5%' }}
-        onClick={() => this.handleSquareClick(x, y)}
-      >
-        <Square black={black}>
-          {piece}
-        </Square>
+      <div key={i}
+          style={{ width: '12.5%', height: '12.5%' }}>
+        <BoardSquare x={x}
+                    y={y}>
+          {this.renderPiece(x, y)}
+        </BoardSquare>
       </div>
     );
   }
 
+  renderPiece(x, y) {
+    const [knightX, knightY] = this.props.knightPosition;
+    if (x === knightX && y === knightY) {
+      return <Knight />;
+    }
+  }
 
   render() {
     const squares = [];
